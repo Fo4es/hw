e44795d0a6c0f53152fa8ffad2d9ad47c6327e08
Fo4es <vlad.zubalii@gmail.com>
2022-07-20 10:11:15 +0300

    hw_6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+
+export default function App() {
+    let state = useSelector(state => state);//Ми звертаємося до 8 рядку в індекс js тобто до поточного стейту
+    let dispatch = useDispatch();// повертаємо посилання на Dispatch з react-redux
 
 
+    return (<div>
+        {state}
+        <button onClick={() => {
+            dispatch({type: "INC", payload: 1});//повертається в 8 рядок js action, де потім type порівнюється(10 рядок js)  з кожним case
+        }}>INCrement
+        </button>
+        <button onClick={() => {
+            dispatch({type: "DEC", payload: 1});//повертається в 8 рядок js action, де потім type порівнюється(10 рядок js)  з кожним case
 
-function App() {
-  return (
-      <div>
+        }}>DECrement
+        </button>
 
-      </div>
-  );
+        <button onClick={() => {
+            dispatch({type: "RES"});//повертається в 8 рядок js action, де потім type порівнюється(10 рядок js)  з кожним case
+
+        }}>Reset</button>
+
+    </div>);
 }
-
-export default App;
